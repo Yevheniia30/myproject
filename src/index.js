0442a88013509a80b84bf7b9a198c2e1ed9a2a7d
Evgeniya3010 <susskaya86@gmail.com>
2020-12-16 00:25:25 +0200

    creates timer plugin

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import createBox from '../js/practice-box';
 import './styles.css';
 import Timer from './js/timer';
 
-const timer = new Timer('#timer');
-console.log('timer:', timer);
+//  TIMER
+const timer = new Timer({
+  selector: '#timer',
+  value: 10,
+});
+
+new Timer({
+  selector: '#timer2',
+  value: 20,
+});
+new Timer({
+  selector: '#timer3',
+  value: 30,
+});
 
 import './js/modal';
 import './js/siema';
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,18 +1,58 @@
 import '../timer.css';
 
-const template = `
+const template = value => `
 <div class="timer card">
-<button class="btn btn-danger stop">STOP</button>
-<span class="value">15</span>
+<button class="btn btn-danger stop" disabled >STOP</button>
+<span class="value">${value}</span>
 <button class="btn btn-success start">START</button>
 </div>`;
 
 class Timer {
-  constructor(selector) {
+  constructor({ selector, value = 0 }) {
     //#timer
     this.container = document.querySelector(selector);
+    this.value = value;
 
-    this.container.insertAdjacentHTML('beforeEnd', template);
+    this.container.insertAdjacentHTML('beforeEnd', template(this.value));
+
+    this.refs = {
+      value: this.container.querySelector('.value'),
+      startBtn: this.container.querySelector('.start'),
+      stopBtn: this.container.querySelector('.stop'),
+    };
+
+    this.refs.stopBtn.addEventListener('click', this.stop.bind(this));
+    this.refs.startBtn.addEventListener('click', this.start.bind(this));
+    // this.start();
+  }
+
+  render() {
+    this.refs.value.textContent = this.value;
+  }
+
+  start() {
+    if (this.timerId) {
+      return;
+    }
+    console.log('start');
+    this.timerId = setInterval(() => {
+      this.value += 1;
+
+      this.render();
+    }, 500);
+    this.refs.stopBtn.removeAttribute('disabled');
+    this.refs.startBtn.setAttribute('disabled', true);
+  }
+
+  stop() {
+    if (!this.timerId) {
+      return;
+    }
+    console.log('stop');
+    clearInterval(this.timerId);
+    this.timerId = 0;
+    this.refs.startBtn.removeAttribute('disabled');
+    this.refs.stopBtn.setAttribute('disabled', true);
   }
 }
 
